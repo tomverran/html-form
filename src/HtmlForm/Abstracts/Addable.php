49b6aa37a2533597d35bb5c92290dbe55e9ab7ab
tom <javaguychronox@gmail.com>
2013-11-25 20:22:42 +0000

    doc comments, cache reflection classes by class name

--- src/HtmlForm/Abstracts/Addable.php
+++ src/HtmlForm/Abstracts/Addable.php
@@ -2,33 +2,71 @@
 
 namespace HtmlForm\Abstracts;
 
+/**
+ * Class Addable
+ * @package HtmlForm\Abstracts
+ *
+ * @method Addable addTextbox($name, $label, $args = array())
+ * @method Addable addEmail($name, $label, $args = array())
+ * @method Addable addNumber($name, $label, $args = array())
+ * @method Addable addRange($name, $label, $min, $max, $args = array())
+ * @method Addable addUrl($name, $label, $args = array())
+ * @method Addable addHidden($name, $label, $args = array())
+ * @method Addable addPassword($name, $label, $args = array())
+ * @method Addable addSelect($name, $label, $options, $args = array())
+ * @method Addable addRadio($name, $label, $options, $args = array())
+ * @method Addable addCheckbox($name, $label, $options, $args = array())
+ * @method Addable addText($name, $html)
+ * @method Addable addHoneypot($args = array())
+ * @method Addable addButton($name, $buttonText, $args = array());
+ * @method Addable addSubmit($name, $buttonText, $args = array());
+ */
 abstract class Addable
 {
 	/**
-	 * Form elements that have been added
+	 * @var array Form elements that have been added
 	 * to the form in sequencial order
 	 */
 	protected $elements = array();
 
-	/**
-	 * Takes care of methods like addTextbox(),
-	 * addSelect(), etc...
-	 * 
-	 * @param  string $method Called method
-	 * @param  array  $args   Arguments passed to the method
-	 * @return self
-	 */
+    /**
+     * @var array A cache of ReflectionClasses
+     */
+    private static $rcCache = array();
+
+    /**
+     * Takes care of methods like addTextbox(),
+     * addSelect(), etc...
+     *
+     * @param  string $method Called method
+     * @param  array $args Arguments passed to the method
+     * @throws \Exception
+     * @return self
+     */
 	public function __call($method, $args)
 	{
 		if ($className = $this->findClass($method)) {
-			$reflect  = new \ReflectionClass($className);
+			$reflect  = self::reflectionClass($className);
 			$element = $reflect->newInstanceArgs($args);
 			$this->elements[] = $element;
 		} else {
-			throw new \Exception("`{$method}()` does not exist on this object.");
+			throw new \BadMethodCallException("`{$method}()` does not exist on this object.");
 		}
 		return $this;
 	}
+
+    /**
+     * Retrieve a reflection class instance for the given class name
+     * @param string $className The class name to get an RC instance for
+     * @return \ReflectionClass
+     */
+    private static function reflectionClass($className)
+    {
+        if (!isset(self::$rcCache[$className])) {
+            self::$rcCache[$className] = new \ReflectionClass($className);
+        }
+        return self::$rcCache[$className];
+    }
 
 	/**
 	 * Based on a passed method name, figure out
